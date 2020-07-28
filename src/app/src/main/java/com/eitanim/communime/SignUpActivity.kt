83d7d968429df7c5c347a1c24c7c93fdc387defb
Adrian Aisemberg <adrianaisemberg@gmail.com>
2020-07-28 13:33:13 -0700

    cleanup

--- src/app/src/main/java/com/eitanim/communime/SignUpActivity.kt
+++ src/app/src/main/java/com/eitanim/communime/SignUpActivity.kt
@@ -1,6 +1,5 @@
 package com.eitanim.communime
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -25,10 +24,7 @@ class SignUpActivity : AppCompatActivity() {
             val userName = findViewById<EditText>(R.id.text_username).text.toString()
             val email = findViewById<EditText>(R.id.text_email).text.toString()
             val password = findViewById<EditText>(R.id.text_password).text.toString()
-            val res  = CommuniMeService.api().signUp(SignUpRequests(userName, email, password))
-
-            var x=0
-            x++
+            CommuniMeService.api().signUp(SignUpRequests(userName, email, password))
         }
     }
 }
--- src/app/src/main/java/com/eitanim/communime/api/CommuniMeApi.kt
+++ src/app/src/main/java/com/eitanim/communime/api/CommuniMeApi.kt
@@ -3,8 +3,6 @@ package com.eitanim.communime.api
 import com.eitanim.communime.api.requests.SignInRequests
 import com.eitanim.communime.api.requests.SignUpRequests
 import com.eitanim.communime.api.responses.ActivityRating
-import okhttp3.Call
-import okhttp3.Request
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -12,7 +10,7 @@ import retrofit2.http.POST
 interface CommuniMeApi {
 
     @POST("/api/users")
-    suspend fun signUp(@Body request: SignUpRequests): Any
+    suspend fun signUp(@Body request: SignUpRequests)
 
     @POST("/api/users")
     suspend fun signIn(@Body request: SignInRequests)
